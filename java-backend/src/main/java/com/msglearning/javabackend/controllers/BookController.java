24baf498d6e50dcd52a33e27daf2a646dd771234
Kaffai Levente <levi.kaffai@gmail.com>
2023-06-11 23:39:32 +0300

    Funcția getCoverImage și getBookById în BookController au fost implementate și funcționează. În rest am creat un package pentru Exceptions și am creat un nou Exception -> ItemNotFoundException. La ControllerConstants am declarat un constant: NOT_FOUND_MESSAGE

--- java-backend/src/main/java/com/msglearning/javabackend/controllers/BookController.java
+++ java-backend/src/main/java/com/msglearning/javabackend/controllers/BookController.java
@@ -1,13 +1,19 @@
 package com.msglearning.javabackend.controllers;
 
 import com.msglearning.javabackend.entity.Book;
+import com.msglearning.javabackend.exceptions.ItemNotFoundException;
 import com.msglearning.javabackend.services.BookService;
 import com.msglearning.javabackend.services.ImageService;
+import org.springframework.core.env.Environment;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
+
+import static com.msglearning.javabackend.controllers.ControllerConstants.*;
 
 @RestController
 @RequestMapping({ControllerConstants.API_PATH_BOOK})
@@ -16,38 +22,54 @@ public class BookController {
     private static final String ID_PATH = "/id/{id}";
     private static final String AUTHOR_PATH = "/author/{name}";
     private static final String SAVE_PATH = "/new";
-    private static final String COVER_IMAGE_PATH = "/covers/{id}";
+    private static final String COVER_IMAGE_PATH = "/cover/{id}";
+
 
     @Autowired
     private BookService bookService;
     @Autowired
     private ImageService imageService;
 
+    @Autowired
+    private Environment env;
+
     @GetMapping(ALL_PATH)
-    public List<Book> getAll()
-    {
+    public List<Book> getAll() {
         return bookService.findAll();
     }
 
     @GetMapping(AUTHOR_PATH)
-    public List<Book> getBooksByAuthor(@PathVariable String name)
-    {
+    public List<Book> getBooksByAuthor(@PathVariable String name) {
         return bookService.findByAuthor(name);
     }
 
-    @PostMapping(SAVE_PATH)
-    public boolean saveBook(@RequestBody Book book)
-    {
-        try{
-            bookService.save(book);
+    @GetMapping(ID_PATH)
+    public Book getBookById(@PathVariable Long id) throws ItemNotFoundException {
+        Optional<Book> opBook = bookService.findById(id);
+        if (opBook.isPresent())
+            return opBook.get();
+        else throw new ItemNotFoundException(NOT_FOUND_MESSAGE + "id=" + id);
+    }
+
+    @GetMapping(value = COVER_IMAGE_PATH, produces = MediaType.IMAGE_JPEG_VALUE)
+    public @ResponseBody
+    byte[] getCoverImage(@PathVariable Long id) throws IOException {
+        Optional<String> imageNameOpt= bookService.getCoverImage(id);
+        if (imageNameOpt.isEmpty()) {
+            return new byte[0];
         }
-        catch (Exception e)
-        {
+        String coverImageStorageSpace = env.getProperty("coverimage.path");
+        return imageService.read(coverImageStorageSpace +"\\"+imageNameOpt.get());
+    }
+    @PostMapping(SAVE_PATH)
+    public boolean saveBook(@RequestBody Book book) {
+        try {
+            bookService.save(book);
+        } catch (Exception e) {
             return false;
         }
         return true;
     }
-
 
 
 }
--- java-backend/src/main/java/com/msglearning/javabackend/controllers/ControllerConstants.java
+++ java-backend/src/main/java/com/msglearning/javabackend/controllers/ControllerConstants.java
@@ -1,11 +1,13 @@
 package com.msglearning.javabackend.controllers;
 
 public class ControllerConstants {
-
+    //Controller constants
     public static final String API_PATH = "/api";
     public static final String API_PATH_USER = API_PATH + "/user";
     public static final String API_PATH_AUTH = API_PATH + "/auth";
-
     public static final String  API_PATH_BOOK = API_PATH + "/book";
+
+    //Messages
+    public static final String NOT_FOUND_MESSAGE = "RECORD NOT FOUND";
 
 }
