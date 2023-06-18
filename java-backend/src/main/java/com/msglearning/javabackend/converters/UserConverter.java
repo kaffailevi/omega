a554b7fc5eda900acf6d3ebe271d486772411147
Timon Janosi <timonj2002@gmail.com>
2023-06-18 19:34:59 +0300

    18.06.2023 - User is manager request + Delete user

--- java-backend/src/main/java/com/msglearning/javabackend/converters/UserConverter.java
+++ java-backend/src/main/java/com/msglearning/javabackend/converters/UserConverter.java
@@ -12,13 +12,12 @@ public class UserConverter {
     }
 
     public static final User convertToEntity(UserTO to) {
-        return new User(to.getId(),null, to.getFirstName(), to.getLastName(), to.getEmail(), to.getPhone(), null, to.getOccupation(),null,null
+        return new User(to.getId(),null, to.getFirstName(), to.getLastName(), to.getEmail(), to.getPhone(), null, to.getOccupation(),false,null,null
         );
     }
 
     public static final User convertToEntity(UserCTO to) {
-        return new User(to.getId(), to.getPassword(), to.getFirstName(), to.getLastName(), to.getEmail(), to.getPhone(), null, to.getOccupation()
-        ,null,null);
+        return new User(to.getId(), to.getPassword(), to.getFirstName(), to.getLastName(), to.getEmail(), to.getPhone(), null, to.getOccupation(),false,null,null);
     }
 
 }
